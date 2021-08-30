a92c611368515991617e0656bad3314d6bbe4481
deepak13031994 <deepak.kumarsah1@gmail.com>
2021-08-30 23:31:48 +0530

    first push on aws

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\LoginController;
-
-
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\sponsor\GenealogyController;
+use App\Http\Controllers\sponsor\PinRequestController;
+use App\Http\Controllers\admin\PackageController;
+use App\Http\Controllers\admin\BankController;
+use App\Http\Controllers\admin\PinsGenerateController;
+use App\Http\Controllers\admin\CustomAdsController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\admin\YoutubeVideosController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,5 +29,34 @@ use App\Http\Controllers\Admin\LoginController;
 |----------------------------------------------------------------------
 */
 
-Route::get('/', [LoginController::class, 'index']);
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::any('/genealogy',[GenealogyController::class, 'viewGenelogy']);
+Route::any('/packages',[PackageController::class, 'viewPackages']);
+Route::any('/generate-pins',[PinsGenerateController::class, 'genaratePins']);
+Route::any('/generate-pins/list',[PinsGenerateController::class, 'genaratePinsList']);
+Route::any('/banks',[BankController::class, 'viewBanks']);
+Route::any('/pin-request',[PinRequestController::class, 'viewPinRequest']);
+Route::any('/pin-request/create',[PinRequestController::class, 'createPinRequest']);
+Route::any('/youtube-videos',[YoutubeVideosController::class, 'youtubeVideos']);
+Route::any('/youtube/channels',[YoutubeVideosController::class, 'youtubeChannels']);
+Route::any('/home',[HomeController::class, 'homeView']);
+Route::any('/transfer-pins',[PinsGenerateController::class, 'transferPins'])->name('transfer-pins');
+Route::any('/requested-pins',[PinRequestController::class, 'requestedPins'])->name('requested-pins');
+Route::any('/requested-pins-action',[PinRequestController::class, 'requestedPinsAction'])->name('requested-pins-action');
+
+
+Route::any('/custom-ads',[CustomAdsController::class, 'CustomAds']);
+Route::any('/custom-ads/edit/{id}',[CustomAdsController::class, 'editCustomAds']);
+Route::any('/custom-ads/update/{id}',[CustomAdsController::class, 'updateCustomAds']);
+Route::any('/custom-ads/delete/{id}',[CustomAdsController::class, 'deleteCustomAds']);
+
+Route::any('/users',[UserController::class, 'usersList']);
+Route::any('/users/{id}',[UserController::class, 'subUsersList']);
+});
+
+
+Route::get('/', [LoginController::class, 'showLoginForm']);
